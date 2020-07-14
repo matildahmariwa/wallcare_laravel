13871ad580808250c053c46eb6d5ab1049b81e2c
unknown <matildamariwa3@gmail.com>
2020-07-14 15:01:31 +0300

    changes

--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -1,162 +1,341 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>Portfolio</title>
-        <link rel="shortcut icon" type="image/x-icon" href="images/logo.png" />
-      
-        <!-- Foundation -->
-        <script src="js2/js/vendor/jquery.js"></script>
-        <script src="js2/js/vendor/what-input.js"></script>
-        <script src="js2/js/vendor/foundation.js"></script>
-        <script src="js2/js/app.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
+<head>
+    <meta charset="UTF-8">
+    <title>portfolio design</title>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700i" rel="stylesheet">
     
-        <link href="js2/css/foundation.css" rel="stylesheet" type="text/css" />
-        <link href="js2/css/app.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">
     
-        <!-- bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.0/jquery.waypoints.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.min.js"></script>
-    <script
-    crossorigin="anonymous"
-    src="https://kit.fontawesome.com/7f19f7dd61.js"></script>  
-    <script src="https://unpkg.com/isotope-layout@3.0.3/dist/isotope.pkgd.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        body{    
+    font-family: 'Roboto Condensed', sans-serif;
+    
+}
+
+        body{
+            overflow: hidden;
+        }
+        
+        
+    .works .filtering .filter {
+  display: inline-block;
+  padding: 2px 10px; }
+        
+        
+.works .filtering span {
+  background: #ebeaef;
+  text-transform: uppercase;
+  cursor: pointer;
+  font-size: 13px;
+  font-weight: 600;
+  margin: 0 5px;
+  padding: 15px 20px;
+  transition: all 0.3s ease 0s;
+}
+        
+        
+  .works .filtering span:hover {
+    background: #eb2f5b;
+    color: #fff; 
+        
+}
+        
+        
+        
+.works .filtering .active {
+  background: #eb2f5b;
+  box-shadow: 0px 8px 30px -5px rgba(0, 0, 0, 0.2);
+  color: #fff; }
+        
+        
+        
+        
+.works .gallery {
+  padding-left: 0;
+  padding-right: 0;
+  margin-bottom: -.05em; 
+}
+        
+        
+        
+.works .larg-padding {
+  padding: 0 15px; }
+        
+        
+        
+  .works .larg-padding .item-img {
+    border: 10px solid #fff;
+    margin-top: 30px; 
+        
+    }
+        
+        
+        
+.works .no-padding {
+  padding: 0; 
+        
+        }
+        
+        
+  .works .no-padding .item-img {
+    margin-top: 0; }
+        
+        
+        
+.works .gutter {
+  padding: 0 10px; }
+        
+        
+  .works .gutter .item-img {
+    margin-top: 20px; }
+        
+        
+        
+.works .item-img {
+  margin-top: 30px;
+  position: relative; }
+        
+        
+  .works .item-img:hover .item-img-overlay {
+    opacity: 1;
+    transform: scale(1, 1); }
+        
+        
+        
+.works .item-img-overlay {
+  background: rgba(0, 0, 0, 0.5);
+  bottom: 0;
+  left: 0;
+  opacity: 0;
+  padding: 30px;
+  position: absolute;
+  right: 0;
+  text-align: center;
+  top: 0;
+  transform: scale(0, 0);
+  transition: all .5s; }
+        
+        
+        
+  .works .item-img-overlay .icon {
+    bottom: 30px;
+    color: #eee;
+    font-size: 30px;
+    height: 30px;
+    line-height: 30px;
+    position: absolute;
+    right: 30px;
+    text-align: center;
+    width: 30px; }
+        
+        
+        
+  .works .item-img-overlay p {
+    color: #f2f2f2;
+    font-weight: 400;
+    margin-bottom: 0; }
+        
+        
+        
+  .works .item-img-overlay h3 {
+    color: #fff;
+    margin: 0;
+    font-size: 25px;
+    line-height: 1.3;
+    font-weight: 500;
+    line-height: 1.3;
+    letter-spacing: 1px;
+    display: inline-block;
+    border-bottom: 3px solid #fff;
+    padding-bottom: 5px;
+    margin-bottom: 20px;
+        
+        
+        }
+        
+       
+    
+    </style>
+</head>
+<body>
+  
+  
  
-    <!--Css styling  -->
-    <link href="css/main.css" rel="stylesheet"/>
-        <script
-          crossorigin="anonymous"
-          src="https://kit.fontawesome.com/7f19f7dd61.js"></script>
+   <section class="works section-padding bg-gray" data-scroll-index="3">
+            <div class="container-fluid">
+               <br><br>
+                <div class="row">
+                   
+
+                    <!-- filter links -->
+                    <div class="filtering text-center mb-30 col-sm-12">
+                        <div class="filter">
+                            <span data-filter='*' class="active">All</span>
+                            <span data-filter='.brand'>Brand</span>
+                            <span data-filter='.design'>Design</span>
+                            <span data-filter='.marketing'>Marketing</span>
+                        </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+                    <br><br><br>
+                    <!-- gallery -->
+                    <div class="gallery full-width">
+
+                        <!-- gallery item -->
+                        <div class="col-lg-4 col-md-6 items no-padding brand">
+                            <div class="item-img">
+                                <img src="images/interior.jpg" alt="image" class="img-responsive">
+                                <div class="item-img-overlay">
+                                    {{-- <div class="overlay-info full-width">
+                                        <p>Logo | Branding</p>
+                                        <h3>Web Design</h3>
+                                        <a href="p1.jpg" class="popimg">
+                                            <span class="icon"><i class="fa fa-search-plus"></i></span>
+                                        </a>
+                                    </div> --}}
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- gallery item -->
+                        <div class="col-lg-4 col-md-6 items no-padding design">
+                            <div class="item-img">
+                              <img src="images/interior.jpg" alt="image" class="img-responsive">
+                                <div class="item-img-overlay">
+                                    <div class="overlay-info full-width">
+                                        <p>Logo | Branding</p>
+                                        <h3>Graphic Forms</h3>
+                                        <a href="p2.jpg" class="popimg">
+                                            <span class="icon"><i class="fa fa-search-plus"></i></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- gallery item -->
+                        <div class="col-lg-4 col-md-6 items no-padding marketing">
+                            <div class="item-img">
+                              <img src="images/render.jpg" alt="image" class="img-responsive">
+                                <div class="item-img-overlay">
+                                    <div class="overlay-info full-width">
+                                        <p>Logo | Branding</p>
+                                        <h3>Creative Design</h3>
+                                        <a href="p3.jpg" class="popimg">
+                                            <span class="icon"><i class="fa fa-search-plus"></i></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- gallery item -->
+                        <div class="col-lg-4 col-md-6 items no-padding brand">
+                            <div class="item-img">
+                              <img src="images/roofing_serve.jpg" alt="image" class="img-responsive">
+                                <div class="item-img-overlay">
+                                    <div class="overlay-info full-width">
+                                        <p>Logo | Branding</p>
+                                        <h3>Project Name</h3>
+                                        <a href="p4.jpg" class="popimg">
+                                            <span class="icon"><i class="fa fa-search-plus"></i></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- gallery item -->
+                        <div class="col-lg-4 col-md-6 items no-padding design">
+                            <div class="item-img">
+                              <img src="images/interior.jpg" alt="image" class="img-responsive">
+                                <div class="item-img-overlay">
+                                    <div class="overlay-info full-width">
+                                        <p>Logo | Branding</p>
+                                        <h3>Seo Analytics</h3>
+                                        <a href="p5.jpg" class="popimg">
+                                            <span class="icon"><i class="fa fa-search-plus"></i></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- gallery item -->
+                        <div class="col-lg-4 col-md-6 items no-padding marketing">
+                            <div class="item-img">
+                              <img src="images/interior.jpg" alt="image" class="img-responsive">
+                                <div class="item-img-overlay">
+                                    <div class="overlay-info full-width">
+                                        <p>Logo | Branding</p>
+                                        <h3>Markting stream</h3>
+                                        <a href="p6.jpg" class="popimg">
+                                            <span class="icon"><i class="fa fa-search-plus"></i></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <script src="isotope.pkgd.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+        <script>
     
-          <!-- Fonts -->
-          <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
-      </head>
-      <body>
-        <nav class="navbar  navbar-expand-lg navbar-light">
-                <div class="container sticky" >
-                    <a class="navbar-brand" href="{{route('/home')}}">
-                        <img src="images/logo.png" alt="wallcare" class="logo">
-                      </a>
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                      </button>
-                  <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto" >
-                      <li class="nav-item active" >
-                        <a class="nav-link"  href="{{route('/home')}}">Home
-                              <span class="sr-only">(current)</span>
-                            </a>
-                      </li>
-                      {{-- <li class="nav-item">
-                        <a class="nav-link" href="#">About Us</a>
-                      </li> --}}
-                      <li class="nav-item">
-                        <a class="nav-link"  href="{{route('services')}}">Services</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link"  href="{{route('portfolio')}}">Portfolio</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>
-              
-              <div class="grid-x services-intro">
-                  <div class="cell large-12 medium-12 small-12">
-                  <h1>
-                    OUR PORTFOLIO
-                  </h1> 
-                 <span><a href="index.html">Home</a> | PORTFOLIO </span> 
-                </div>
-                </div>
-              <!-- end of intro -->
-              <div>
-              <div>
-                  <section id="portfolio" class="bg-light-gray">
-                      <div class="gallary animate-grid">
-                          <div class="container">
-                              <div class="row">
-                                  <div class="col-lg-12 text-center">
-                                      <h2 class="section-heading">Portfolio</h2>
-                                      <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                                  </div>
-                              </div>
-                                  <div class="row">
-                                  <div class="col-xs-12">
-                                      <div class="categories">
-                                          <ul>
-                                              <li>
-                                                  <ol>
-                                                      <li><a href="#" data-filter="*" class="active">All</a></li>
-                                                      <li><a href="#" data-filter=".architecture">Architecture</a></li>
-                                                      <li><a href="#" data-filter=".fashion">Fashion</a></li>
-                                                      <li><a href="#" data-filter=".land">Landscape</a></li>
-                                                      <li><a href="#" data-filter=".food">Food</a></li>
-                                                  </ol>
-                                              </li>
-                                          </ul>
-                                      </div>
-                                  </div>
-                              </div>
-              </div>
-              <div class="grid-x social">
-                  <div class="cell large-12 small-12">
-                   <ul>
-                       <li><a href="https://www.facebook.com/wallcareltd/"><i class="fab fa-facebook-f "></i></a></li></li>
-                       <li><a href="https://www.instagram.com/wallcare_contractors/"><i class="fab fa-instagram "></i></a></li>
-                       <li><a href="https://twitter.com/WallcareC"><i class="fab fa-twitter"></i></a></li>
-                       <li> <a href="https://www.linkedin.com/in/wallcare-contractors-988235184/?originalSubdomain=ke"><i class="fab fa-linkedin-in "> </a></i></li>
-                   </ul>
-                  </div>
-                 </div> 
-                 <script>
-                 jQuery(function() {
-                  jQuery('#showall').click(function() {
-                    jQuery('.targetDiv').show();
-                  });
-                  jQuery('.showSingle').click(function() {
-                    jQuery('.targetDiv').hide();
-                    jQuery('#div' + $(this).attr('target')).show();
-                  });
-                });  
-              </script> 
-               <script>
-                $('.buttons a').click(function(){
-                  $('a').removeClass('active');
-                  $(this).addClass('active');
-                });
-                      
-                </script> 
-                <script>
-    $(window).load(function() {
-    var $container = $('.animate-grid .gallary-thumbs');
-    $container.isotope({
-        filter: '*',
-        transitionDuration: '0.6s',
+    
+   $(function () {
+ 
+          
+     $('.gallery').magnificPopup({
+        delegate: '.popimg',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
     });
-    $('.animate-grid .categories a').click(function() {
-        $('.animate-grid .categories .active').removeClass('active');
-        $(this).addClass('active');
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector
-        });
-        return false;
+      $('.gallery').isotope({
+      // options
+      itemSelector: '.items'
     });
-});
-                </script>
-  </body>
+
+    var $gallery = $('.gallery').isotope({
+      // options
+    });
+
+    // filter items on button click
+    $('.filtering').on( 'click', 'span', function() {
+
+        var filterValue = $(this).attr('data-filter');
+
+        $gallery.isotope({ filter: filterValue });
+
+    });
+
+    $('.filtering').on( 'click', 'span', function() {
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+
+       
+}); 
+
+    
+       </script>
+  
+</body>
 </html>
